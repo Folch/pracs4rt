33e7f62cf97a355400dd37d796f421dfb6978ad0
xaviml <xaviml.93@gmail.com>
2014-11-08 17:15:46 +0100

    lz77 coding in C, first steps

---
+++ tm/Exercicis/EX4/lz77/compressor.c
@@ -0,0 +1,11 @@
+#include "compressor.h"
+
+void search(char* text, char* pattern, CompressStruct* cs) {
+
+}
+
+char *compress(char* input, int llis_size, int ent_size) {
+
+	return (char*) malloc(7*sizeof(char));
+}
+
---
+++ tm/Exercicis/EX4/lz77/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+#include "compressor.h"
+#include "decompressor.h"
+
+int size_ent = 8;
+int size_llis = 4;
+
+void parse_opt(int argc,char *argv[]) {
+	int c;
+	static struct option long_options[] = {
+		{"size_ent",     required_argument, 0, 'e'},
+		{"size_llis",  required_argument, 0, 'l'},
+		{0, 0, 0, 0}
+	};
+	
+
+	while (1) {
+		/* getopt_long stores the option index here. */
+		int option_index = 0;
+
+		c = getopt_long (argc, argv, "e:l:",
+				long_options, &option_index);
+
+		/* Detect the end of the options. */
+		if (c == -1) break;
+
+		switch (c) {
+
+			case 'e':
+				size_ent = atoi(optarg);
+				break;
+			case 'l':
+				size_llis = atoi(optarg);
+				break;
+
+			default:
+				abort();
+		 }
+	}
+}
+
+int main(int argc,char *argv[]) {
+	
+	
+	parse_opt(argc, argv);
+	
+
+	char * in = malloc(7*sizeof(char));
+	strcpy(in,"111010");
+	
+	
+
+	printf("%s\n", in);
+	printf("%d entrada\n", size_ent);
+	printf("%d lliscant\n", size_llis);
+	return 0;
+}
