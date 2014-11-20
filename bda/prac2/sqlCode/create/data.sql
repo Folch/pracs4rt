0594523a61c704b68af0ee6257851d99269afde0
Folch <albertfolch58@hotmail.com>
2014-11-21 00:52:22 +0100

    Exercise 5 done

--- bda/prac2/sqlCode/create/data.sql
+++ bda/prac2/sqlCode/create/data.sql
@@ -1,13 +1,15 @@
-SELECT
-	datum AS Date,
-	extract(year FROM datum) AS Year,
-	extract(month FROM datum) AS Month,
-	extract(day FROM datum) AS Day
+INSERT INTO Data (id_any,id_mes,id_dia) 
+VALUES(
+	SELECT
+		extract(year FROM datum) AS Year,
+		extract(month FROM datum) AS Month,
+		extract(day FROM datum) AS Day
 	
-FROM (
-	-- There are 3 leap years in this range, so calculate 365 * 10 + 3 records
-	SELECT '2000-01-01'::DATE + sequence.day AS datum
-	FROM generate_series(0,3652) AS sequence(day)
-	GROUP BY sequence.day
-     ) 
-ORDER BY 1;
+	FROM (
+		-- There are 3 leap years in this range, so calculate 365 * 10 + 3 records
+		SELECT '2000-01-01'::DATE + sequence.day AS datum
+		FROM generate_series(0,3652) AS sequence(day)
+		GROUP BY sequence.day
+	     ) DQ
+	ORDER BY 1
+);
