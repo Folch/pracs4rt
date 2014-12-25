b74f7e178e82faee9bb1b41b568f75626a5df1db
Folch <albertfolch58@hotmail.com>
2014-12-25 11:49:38 +0100

    getNumTeseles and getPosicioTesela done

--- tm/ZipMediaPlayer/src/model/Imatge.java
+++ tm/ZipMediaPlayer/src/model/Imatge.java
@@ -11,14 +11,16 @@ import java.awt.image.WritableRaster;
 
 /**
  * Classe que emmagatzema un BufferedImage i el seu nom
+ *
  * @author Albert Folch i Xavi Moreno
  */
-public class Imatge implements Cloneable{
+public class Imatge implements Cloneable {
+
     private String name;
     private BufferedImage image;
-    
-    public Imatge(){
-        
+
+    public Imatge() {
+
     }
 
     public String getName() {
@@ -36,9 +38,11 @@ public class Imatge implements Cloneable{
     public void setImage(BufferedImage image) {
         this.image = image;
     }
+
     /**
      * Mètode que retorna un clon del buffered image que conté aquesta imatge
-     * @return 
+     *
+     * @return
      */
     public BufferedImage deepCopy() {
         ColorModel cm = image.getColorModel();
@@ -46,27 +50,48 @@ public class Imatge implements Cloneable{
         WritableRaster raster = image.copyData(null);
         return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
     }
-    
+
     /**
      * Mètode de sobreescritura del clone
+     *
      * @return
-     * @throws CloneNotSupportedException 
+     * @throws CloneNotSupportedException
      */
     @Override
     protected BufferedImage clone() throws CloneNotSupportedException {
         super.clone();
         return deepCopy();
     }
-    
+
+    /**
+     * Mètode que donada una tesela t i una mida size_t retorna la posició
+     *
+     * @param t
+     * @param size_t
+     * @return pos[0]=x,columnes pos[1]=y,files
+     */
     public Integer[] getPosTesela(int t, int size_t) {
-        
-        return null;
+
+        int teselesPerFila = this.image.getWidth() / size_t;
+        Integer[] pos = new Integer[2];
+
+        int fila = t / teselesPerFila;
+        int columna = t % teselesPerFila;
+
+        pos[0] = columna * size_t;
+        pos[1] = fila * size_t;
+        return pos;
     }
-    
+
+    /**
+     * Mètode que retorna el número de teseles de mida size_t*size_t
+     *
+     * @param size_t
+     * @return
+     */
     public int getNumTeseles(int size_t) {
-        
-        return 0;
+
+        return (this.image.getHeight() * this.image.getWidth()) / (size_t * size_t);
     }
-    
-    
+
 }
